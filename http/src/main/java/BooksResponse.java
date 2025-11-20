5f13f08f5d393b648bebfe308ebee5f02f5ba563
Dudu <tigrao.dobem1@gmail.com>
2025-11-20 07:38:02 -0300

    primeiro exercicios de requsição http estruturado

---
+++ http/src/main/java/BooksResponse.java
@@ -0,0 +1,6 @@
+import java.util.List;
+
+public record BooksResponse(List<Item> items) {
+    public record Item(volumeInfo volumeInfo) {}
+
+}
---
+++ http/src/main/java/volumeInfo.java
@@ -0,0 +1,5 @@
+import java.util.List;
+
+public record volumeInfo(String title, String subtitle, List<String> authors, String publisher, String publishedDate, String description) {
+
+}
